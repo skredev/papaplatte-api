3098671ace335310b7af90b86b38bbf0af08e7b3
Samuel Krebs <hi@skre.dev>
2024-08-27 00:32:01 +0200

    comments

--- lib/api-utils.ts
+++ lib/api-utils.ts
@@ -4,10 +4,12 @@ import * as path from 'path';
 const imageFolder = fs.readdirSync(path.join(__dirname, "../../../public/images"))
 const imageFolderPath = "../../../images"
 
+// Returning the image folder path
 export function getImageFolderPath() {
     return imageFolderPath
 }
 
+// Returning random Image ID
 export function randomImage() {
     let amountImages: number = 0;
     imageFolder.forEach((file: string) => {
@@ -17,6 +19,7 @@ export function randomImage() {
     return Math.floor(Math.random() * amountImages)
 }
 
+//  Returning the current amount of images in the folder
 export function imageAmount() {
     let amountImages: number = 0;
     imageFolder.forEach((file: string) => {
